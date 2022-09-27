2b7460c59bec54514866f48b447392d6e5185d7b
Jasper Oudemans <jasperoudemans@gmail.com>
2022-09-27 15:11:07 -0400

    changed contact me page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,9 +8,22 @@ const Contact = () => {
         <p>View my portfolio projects on GitHub</p>
         <p>
           I am a self-motivated, natural problem solver who loves working on
-          projects with a team, looking for a junior developer position where I
-          can learn
+          projects with a team, looking for an internship or junior developer
+          position where I can learn.
         </p>
+        <ul className="contact-info2">
+          <li>
+            <a href="mailto:jasperoudemans@gmail.com">
+              jasperoudemans@gmail.com
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/jasperoudemans">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/jasper-oudemans/">LinkedIn</a>
+          </li>
+        </ul>
       </div>
     </ProfileSection>
   );
