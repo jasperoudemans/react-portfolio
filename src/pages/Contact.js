73df9e1ff4987d30c4e7991af50a6d03638dc57c
Jasper Oudemans <jasperoudemans@gmail.com>
2022-09-27 14:55:52 -0400

    changed styles

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,8 +7,9 @@ const Contact = () => {
         <p>Contact me via email or LinkedIn at the links provided below</p>
         <p>View my portfolio projects on GitHub</p>
         <p>
-          I am a motivated who loves working on projects with a team, looking
-          for a junior developer position where I can learn
+          I am a self-motivated, natural problem solver who loves working on
+          projects with a team, looking for a junior developer position where I
+          can learn
         </p>
       </div>
     </ProfileSection>
